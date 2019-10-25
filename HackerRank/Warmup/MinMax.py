5a91ff409a8bb4dfefba911cd28de4153f0748fd
Castor <test@thisisonlyatest.com>
2019-10-25 19:07:36 +0530

    MinMax

---
+++ HackerRank/Warmup/MinMax.py
@@ -0,0 +1,12 @@
+def miniMaxSum(arr):
+    min=arr[0]
+    max=arr[0]
+    sum=0
+    for i in arr:
+        if i>max:
+            max=i
+        if i < min:
+            min=i
+        sum+=i
+    print(sum-max, end=" ")
+    print(sum-min)
